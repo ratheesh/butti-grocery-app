2c289c90f17874c74f4784afcffbbf5d7c193725
Ratheesh <ratheeshreddy@gmail.com>
2023-09-17 14:54:10 +0000

    feat(frontend): Add basic signup form

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,10 +1,59 @@
 <template>
     <div class="container">
-        <h2 class="text-center">Signup</h2>
-    </div> 
+        <div class="row justify-content-md-center mt-5">
+            <div class="col-md-5">
+            <div class="card m-auto">
+                <h2 class="text-center my-3">Signup</h2>
+                <hr>
+                <div class="card-body m-0 p-0">
+                    <form m-0>
+                        <div class="mb-3 px-3">
+                            <label for="username" class="form-label">User Name</label>
+                            <input type="text" id="username" class="form-control" placeholder="User Name" />
+                        </div>
+                        <div class="mb-3 px-3">
+                            <label for="email" class="form-label">E-Mail</label>
+                            <input type="text" id="email" class="form-control" placeholder="E-mail address" />
+                        </div>
+                        <div class="mb-3 px-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" id="password" class="form-control" placeholder="Password" />
+                        </div>
+                        <div class="mb-3 px-3">
+                            <label for="password2" class="form-label">Re-enter Password</label>
+                            <input type="password2" id="password" class="form-control" placeholder="Password" />
+                        </div>
+                        <hr class="mt-3 mx-0">
+                        <div class="mb-3 text-center">
+                            <button type="submit" @click.prevent="handleSignup" class="btn btn-success">Signup</button>
+                            &nbsp;
+                            <span class="text-muted">Already a Member?</span>
+                            &nbsp;
+                            <a @click.prevent="handleLogin" href="#">Login</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
-<script setup></script>
+<script setup>
+import router from '@/router';
+
+
+const handleSignup = () => {
+    console.log('Signup');
+    router.push('/signup');
+}
+
+const handleLogin = () => {
+    console.log('Login');
+    router.push('/login');
+}
+
+</script>
 
 <style scoped></style>
