154a7b1511c584929c1cd6c5657e04e46b754d73
Ratheesh S <ratheeshreddy@gmail.com>
2023-09-18 23:54:21 +0530

    frontend: Add adminview and manager view components

---
+++ src/views/AdminView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2 class="text-center">Admin Dash Board!</h2>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/ManagerView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2 class="text-center">Manager Dashboard</h2>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
