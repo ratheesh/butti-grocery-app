73ad93a58d4b7ce87be7a2a69ad75dc34a135bee
Ratheesh S <ratheeshreddy@gmail.com>
2023-09-23 19:45:11 +0530

    feat(frontend): rename views->pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
   const routes = [
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/HomeView.vue')
+      component: () => import('@/pages/HomePage.vue')
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('@/views/SignupView.vue')
+      component: () => import('@/pages/SignupPage.vue')
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/LoginView.vue')
+      component: () => import('@/pages/LoginPage.vue')
     },
     {
       path: '/cart',
       name: 'cart',
-      component: () => import('@/views/CartView.vue')
+      component: () => import('@/pages/CartView.vue')
     },
   ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
