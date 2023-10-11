1383f46cd503ad40774180e73a21e355849adb7e
Ratheesh S <ratheeshreddy@gmail.com>
2023-10-11 22:51:54 +0530

    frontend: cart page changes
    * Use localstorage for storing authentication info
    * Use index based cart item removal
    * Deal with empty cart

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,7 +1,8 @@
 <template>
   <main-layout>
     <!-- <pre>{{ cart.items }}</pre> -->
-    <h2 class="text-center">Cart</h2> 
+    <div v-if="cart.items.length > 0">
+    <h2 class="text-center">Your Cart</h2> 
     <div class="row justify-content-center m-auto">
       <div class="col-8">
         <hr/>
@@ -17,7 +18,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for='item in cart.items' :id="item.id">
+            <tr v-for='(item, idx) in cart.items' :id="item.id">
             <th>{{ item.id }}</th>
             <th>{{ item.item }}</th>
             <th>
@@ -31,7 +32,7 @@
             </th>
             <th>₹{{ item.unit_price }}/-</th>
             <th>₹{{ item.quantity * item.unit_price }}/-</th>
-            <th><mdicon name="delete" class="text-secondary" @click="deleteItem(item.id)" /></th>
+            <th><mdicon name="delete" class="text-secondary" @click="deleteItem(idx)" /></th>
             </tr>
             <tr>
             <th></th>
@@ -45,7 +46,7 @@
             <th></th>
             <th></th>
             <th></th>
-            <th style='text-right'><button type="button" class="btn btn-secondary"><mdicon name="home" :height='18'/>Goto Home</button></th>
+            <th style='text-right'><button type="button" class="btn btn-secondary" @click="router.push('/')"><mdicon name="home" :height='18'/>Goto Home</button></th>
             <th><button type="button" class="btn btn-success ml-auto">Order</button></th>
             <th></th>
             </tr>
@@ -53,14 +54,20 @@
         </table>
       </div>
       </div>
+  </div>
+  <div v-else>
+    <h2 class="text-center mt-5 ">Your Cart is Empty!</h2>
+  </div>
   </main-layout>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 import { useCartStore } from '@/stores/cartstore.js';
+import { useRouter } from 'vue-router';
 import MainLayout from '@/layouts/MainLayout.vue';
 
+const router = useRouter();
 const cart = useCartStore()
 const total = computed(() => cart.items.reduce((y, x) => y+=(x.quantity * x.unit_price), 0))
 
--- src/stores/authstore.js
+++ src/stores/authstore.js
@@ -25,36 +25,36 @@ export const useAuthStore = defineStore('authStore', () => {
   async function login(username, password) {
     try {
       const res = await axiosClient.post('/login', { username, password })
-      console.log(res)
-      if (res.data.error === 200) {
+      // console.log(res)
+      if (res.status === 200) {
         authenticated.value = true
         role.value = res.data.user.role
         user.value = res.data.user
+        localStorage.setItem('access_token', JSON.stringify(res.data.access_token))
         console.log('Logged in!')
         return res
       } else {
         return res
       }
     } catch (err) {
-      // console.log(err.response.status)
-      // console.log(err.response.data)
-
       return err.response
-      // console.log(err.response.data.error)
     }
   }
 
   async function logout() {
-    try {
-      const res = await axiosClient.post('/logout')
-      console.log(res)
-      authenticated.value = false
-      role.value = 'user'
-      user.value = {}
-      console.log('logged out')
-    } catch (err) {
-      console.log(err)
-    }
+    localStorage.removeItem('access_token')
+    console.log('logged out')
+    authenticated.value = false
+    // try {
+    //   const res = await axiosClient.post('/logout')
+    //   console.log(res)
+    //   authenticated.value = false
+    //   role.value = 'user'
+    //   user.value = {}
+    //   console.log('logged out')
+    // } catch (err) {
+    //   console.log(err)
+    // }
   }
 
   async function signup(user_data) {
