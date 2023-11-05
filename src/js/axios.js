98e8d2c8e31fd4d359b3b7c6bff812929ef735f1
Ratheesh <ratheeshreddy@gmail.com>
2023-11-05 11:39:57 +0000

    chore: refactor main.js and axios.js

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5000',
-    withcredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-      common: {
-        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-      }
+  baseURL: 'http://localhost:5000',
+  withcredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    common: {
+      Authorization: `Bearer ${localStorage.getItem('access_token')}`
     }
-  });
-  
+  }
+})
+
 export default axiosClient
