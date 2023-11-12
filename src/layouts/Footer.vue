671139914c7b936c5ef367f30aa2d46404341865
Ratheesh S <ratheeshreddy@gmail.com>
2023-11-12 10:29:30 +0530

    frontend: adjust footer height

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 <footer >
-  <div class="footer fixed-bottom" style="color: #fff; background-color: #34495e">
+  <div class="footer fixed-bottom mt-3" style="height:1.5rem;color: #fff; background-color: #34495e">
     <p class="text-center footer-text">
       © <span class="logo">Butti</span> Groceries by Ratheesh
     </p>
