70f2ba1162160abd60f81ddbc90527ec6d6cfb61
Ratheesh S <ratheeshreddy@gmail.com>
2023-11-14 22:06:07 +0530

    frontend: modify products diplayed on home based on the backend data

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,19 +2,19 @@
   <div>
     <div class="card mb-3 h-100" style="width: 16rem">
       <img
-        class="card-img-top object-fit-cover shadow"
-        :src="data.image"
-        alt="apples"
+        class="card-img-top object-fit-contain shadow"
+        :src="`${backend_url_base}/${data.image_name}`"
+        alt="product image"
         height="150"
       />
       <div class="card-body">
         <div class="inline-flex justify-content-between">
           <div class="row">
             <div class="col">
-              <b class="fs-6">Apples</b>
+              <b class="fs-6">{{ data.name }}</b>
             </div>
             <div class="col">
-              <span class="text-dark fw-bold fs-6"><b>₹</b>{{ data.price }}</span>
+              <span class="text-dark fw-normal fs-6"><b>₹</b><b>{{ data.price }}</b>/{{ data.unit }}</span>
             </div>
           </div>
         </div>
@@ -24,7 +24,7 @@
           <div class="row text-center d-inline-flex align-items-center">
             <div class="col d-flex align-items-center">
               <mdicon @click="updateCount()" name="minus-circle" class="text-danger"/>
-              <label class="text-center" style="width: 2em">{{ data.quantity }}</label>
+              <label class="text-center" style="width: 2em">{{ quantity }}</label>
               <mdicon
                 @click="updateCount(true)"
                 name="plus-circle"
@@ -32,7 +32,7 @@
               />
             </div> 
             <div class="col">
-              <form @submit.prevent="addToCart">
+              <form @submit.prevent="addToCart(data)">
                 <button class="btn btn-success btn-sm">
                   <mdicon name="cart-plus" :size="20"/>Add
                 </button>
@@ -41,34 +41,66 @@
           </div>
         </div>
       </div>
+      <!-- <pre>{{ data }}</pre> -->
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { ref, toRef } from "vue";
+import { useCartStore } from '@/stores/cartstore.js';
 
-const data = reactive({
-  id: 1,
-  name: "Apples",
-  price: 1.99,
-  description: "Kashmir Apples",
-  // image: 'https://images.squarespace-cdn.com/content/v1/5b5b5824f2e6b10639fdaf09/1651599956690-0CE43F9HKEXIEK89CY0B/1.png',
-  image: "https://picsum.photos/200/300?random=1",
-  quantity: 0,
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
 });
+
+const backend_url_base = "http://localhost:5000/images/products";
+const data = toRef(props, 'data')
+const quantity=ref(1)
 
 const updateCount = (add) => {
   if (add) {
-    if (data.quantity < 0) data.quantity = 0;
-    data.quantity++;
+    if (quantity.value < 1) quantity.value = 1;
+    quantity.value++;
   } else {
-    if (data.quantity < 0) data.quantity = 0;
-    if (data.quantity > 0) {
-      data.quantity--;
+    if (quantity.value < 1) quantity.value = 1;
+    if (quantity.value > 1) {
+      quantity.value--;
     }
   }
 };
+
+const cart = useCartStore()
+function addToCart (product) {
+  if (quantity.value < 1) {
+    console.log('addToCart: Quantity cannot be zero')
+    return
+  }
+
+  // if the added item is already in the cart, update the quantity
+  const _item = cart.items.find(item => item.id === product.id)
+  if (_item) {
+    const index = cart.items.indexOf(_item)
+    // cart.items.splice(index, 1, _item)
+    const item = cart.items[index]
+    item.quantity += quantity.value
+    return
+  }
+
+  let item = {}
+  item.id = product.id
+  item.name = product.name
+  item.unit = product.unit
+  item.unit_price = product.price
+  item.quantity = quantity.value
+  
+  cart.items.push(item)
+
+}
+
 </script>
 
 <style scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,61 +1,10 @@
 <template>
   <main-layout>
-  <div class="main text-center m-auto">
+  <div class="main mt-3">
     <div class="container">
-      <!-- <h1 class="text-center my-3">Products</h1> -->
-      <div
-        class="row row-cols-1 row-cols-sm-2 row-cols-md-3 rows-cols-lg-3 rows-cols-xl-4 row-cols-xxl-4 g-1"
-      >
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
-        </div>
-        <div class="col">
-          <ProductCard />
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 rows-cols-lg-3 rows-cols-xl-4 row-cols-xxl-4 g-1" >
+        <div v-for="product in products" class="col" :key="product.id">
+          <ProductCard :data="product"/>
         </div>
       </div>
     </div>
@@ -66,4 +15,22 @@
 <script setup>
 import MainLayout from "@/layouts/MainLayout.vue";
 import ProductCard from "@/components/ProductCard.vue";
+
+import { ref, onMounted } from "vue";
+import axiosClient from '@/js/axios.js'
+
+const products=ref([])
+onMounted(async() => {
+  console.log('Fetching products')
+  axiosClient.get('/api/product')
+    .then(res => {
+      console.log(res)
+      products.value = res.data
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
+
+
 </script>
