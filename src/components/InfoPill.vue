c6c1bdc41aa66de3e977290a16b1121fe6f68c0d
Ratheesh S <ratheeshreddy@gmail.com>
2023-12-13 20:09:20 +0530

    Add previously missed info pill

---
+++ src/components/InfoPill.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <div class="container">
+        <div class="row col-8 justify-content-center align-items-baseline">
+            <div class="card card-body border border-top-0 border-bottom-0 border-end-0 border-success-subtle border-5 m-0 p-0 py-3"
+            style="width:10rem">
+                <div class="row d-flex justify-content-center">
+                    <div class="row d-flex align-items-center">
+                        <mdicon class="col-auto" :name="icon" :size="30" :color="color" />
+                        <span class="col-auto fw-bold fs-5 p-0">{{ title }}</span>
+                        <span class="col-auto ms-2 fs-6 text-center">{{ data }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: String,
+        required: true
+    }
+})
+
+const { title, icon, color, data } = toRefs(props)
+</script>
