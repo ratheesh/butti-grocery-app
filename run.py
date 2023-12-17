ec80c4a14603991d7a4ae68a01f0868b5de8979b
Ratheesh S <ratheeshreddy@gmail.com>
2023-12-17 18:47:05 +0530

    remove stale run.py file

--- run.py
+++
@@ -1,8 +0,0 @@
-# from backend import create_app
-from backend import app
-
-celery = app.extensions["celery"]
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-# End of File
